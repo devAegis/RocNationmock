6491ba93aeb8a947cf0c44939a883224609d7d23
Rufaro O Mandipa <mandipaobey@gmail.com>
2025-03-12 14:10:44 +0200

    add dropdown menu to 3 linkes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,21 +4,37 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className='flex justify-around bg-black h-[120px] items-center'>
+    <nav className='flex justify-around bg-black h-[120px] items-center w-full'>
       <div className='text-white text-[30px] font-bold font-sans group-hover:text-black'>ROCKNATION</div>
       <ul className='flex text-white text-[20px] font- space-x-6'>
         <li className='hover:border-b-2 border-white h-[40px]'>
           <Link to="/" className=''>Home</Link>
         </li>
-        <li>
+        <li className='relative group'>
           <Link to="/Music" className=''>Music</Link>
+          {/* Dropdown Menu */}
+          <div className='absolute w-screen ml-[-658px] align-left hidden bg-blue-300 text-white mt-[50px]  h-[40vh] p-2 shadow-lg group-hover:block '>
+           
+          </div>
         </li>
-        <li className=''>
+        <li className='relative group'>
           <Link to="/Sports" className=' '>Sports</Link>
+          {/* Dropdown Menu */}
+          <div className='absolute w-screen ml-[-739px] align-left hidden bg-blue-300 text-white mt-[50px]  h-[40vh] p-2 shadow-lg group-hover:block '>
+           
+          </div>
         </li>
-        <li>
-          <Link to="/News" className=''>News</Link>
+
+        <li className='relative group'>
+          <Link to="/News" className=''>
+            News
+          </Link>
+          {/* Dropdown Menu */}
+          <div className='absolute w-screen ml-[-820px] align-left hidden bg-blue-300 text-white mt-[50px]  h-[40vh] p-2 shadow-lg group-hover:block '>
+           
+          </div>
         </li>
+
         <li className='hover:border-b-2 border-white h-[40px]'>
           <Link to="/Discover" className=''>Discover</Link>
         </li>
