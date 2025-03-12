6bb54b19a37e5b43e096c0a3524463009f509870
Rufaro O Mandipa <mandipaobey@gmail.com>
2025-03-12 13:05:21 +0200

    add stylings to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,43 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/Music">Music</Link></li>
-        <li><Link to="/Sports">Sports</Link></li>
-        <li><Link to="/News">News</Link></li>
-        <li><Link to="/Discover">Discover</Link></li>
-        <li><Link to="/About">About</Link></li>
-        <li><Link to="/ShopPlanes">Shop Planes</Link></li>
-        <li><Link to="/Store">Store</Link></li>
-
+    <nav className='flex justify-around bg-black h-[120px] items-center'>
+      <div className='text-white text-[30px] font-bold font-sans group-hover:text-black'>ROCKNATION</div>
+      <ul className='flex text-white text-[20px] font- space-x-6'>
+        <li className='hover:border-b-2 border-white h-[40px]'>
+          <Link to="/" className=''>Home</Link>
+        </li>
+        <li>
+          <Link to="/Music" className=''>Music</Link>
+        </li>
+        <li className=''>
+          <Link to="/Sports" className=' '>Sports</Link>
+        </li>
+        <li>
+          <Link to="/News" className=''>News</Link>
+        </li>
+        <li className='hover:border-b-2 border-white h-[40px]'>
+          <Link to="/Discover" className=''>Discover</Link>
+        </li>
+        <li className='hover:border-b-2 border-white h-[40px]'>
+          <Link to="/About" className=''>About</Link>
+        </li>
+        <li className='hover:border-b-2 border-white h-[40px]'>
+          <Link to="/ShopPlanes" className=''>Shop Planes</Link>
+        </li>
+        <li className='hover:border-b-2 border-white h-[40px]'>
+          <Link to="/Store" className=''>Store</Link>
+        </li>
       </ul>
+      <div className='flex items-center border-b-2 border-b-white'>
+      <input
+        type="text"
+        placeholder=""
+        className='h-[50px] w-[320px] px-2 focus:outline-none  bg-transparent text-white '
+      />
+      <img src='' alt='ser' className='bg-white rounded-full h-[40px] w-[40px]'/>
+      </div>
+      
     </nav>
   );
 }
