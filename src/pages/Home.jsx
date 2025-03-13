95a328c23168019a972a3589e8a5add5566bcb30
Rufaro O Mandipa <mandipaobey@gmail.com>
2025-03-13 17:16:56 +0200

    implement carousel in section one

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,44 +5,75 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <div className="relative carousel w-full mt-[40px]">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-            className="w-full" />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide4" className="btn btn-circle">❮</a>
-            <a href="#slide2" className="btn btn-circle">❯</a>
+      
+
+      <section>
+        <div className="relative carousel w-full">
+          <div id="slide1" className="carousel-item relative w-full h-[80vh] bg-black ">
+            <img
+              src="https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg?auto=compress&cs=tinysrgb&w=600"
+              className="w-[50%] mx-auto" aling-center/>
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a href="#slide4" className=" border-none bg-transparent text-white text-[24px] m-">❮</a>
+              <a href="#slide2" className="btn btn-circle">❯</a>
+            </div>
+          </div>
+          <div id="slide2" className="carousel-item relative w-full h-[80vh] bg-black">
+            <img
+              src="https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=600"
+              className="w-[50%] mx-auto" />
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a href="#slide1" className="btn btn-circle">❮</a>
+              <a href="#slide3" className="btn btn-circle">❯</a>
+            </div>
+          </div>
+          <div id="slide3" className="carousel-item relative w-full h-[80vh] bg-black">
+            <img
+              src="https://images.pexels.com/photos/12602140/pexels-photo-12602140.jpeg?auto=compress&cs=tinysrgb&w=600"
+              className="w-[50%] mx-auto" />
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a href="#slide2" className="btn btn-circle">❮</a>
+              <a href="#slide4" className="btn btn-circle">❯</a>
+            </div>
+          </div>
+          <div id="slide4" className="carousel-item relative w-full h-[80vh] bg-black">
+            <img
+              src="https://images.pexels.com/photos/210019/pexels-photo-210019.jpeg?auto=compress&cs=tinysrgb&w=600"
+              className="w-[50%] mx-auto " />
+            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+              <a href="#slide3" className="btn btn-circle">❮</a>
+              <a href="#slide1" className="btn btn-circle">❯</a>
+            </div>
           </div>
         </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-            className="w-full" />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide1" className="btn btn-circle">❮</a>
-            <a href="#slide3" className="btn btn-circle">❯</a>
-          </div>
+      </section>
+
+      <section>
+        <div className='flex justify-between m-auto mt-[60px] w-[80%]'>
+          <p>About ROCKNATION</p>
+          <p>Listen & Subscribe</p>
         </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-            className="w-full" />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide2" className="btn btn-circle">❮</a>
-            <a href="#slide4" className="btn btn-circle">❯</a>
+
+        <div className='flex justify-between m-auto mt-[10px] w-[80%]'>
+          <div className='bg-blue-300 h-[60vh] w-1/3'>
+            
           </div>
+          <div className='bg-black h-[60vh] w-1/3'></div>
+          <div className='bg-blue-400 h-[60vh] w-1/3'></div>
         </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-            className="w-full" />
-          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-            <a href="#slide3" className="btn btn-circle">❮</a>
-            <a href="#slide1" className="btn btn-circle">❯</a>
-          </div>
-        </div>
-      </div>
+      </section>
+
+      <section></section>
+
+      <section></section>
+
+      <section></section>
+
+      <section></section>
+
+      <section></section>
+
+      <section></section>
     </div>
   )
 }
